39b4a9fbf78f56bb5102d9f37497bddbd5cd68d1
Pkarki0 <hi2carkey6@gmail.com>
2024-11-28 20:34:46 -0500

    exported images

--- benefit-connect-client/src/images.js
+++ benefit-connect-client/src/images.js
@@ -4,7 +4,44 @@ import image3 from "./assets/healthcare3.png";
 import image4 from "./assets/healthcare4.png";
 import image5 from "./assets/healthcare5.png";
 import image6 from "./assets/healthcare6.png";
+import image7 from "./assets/healthcare7.png";
+import image8 from "./assets/healthcare8.png";
+import image9 from "./assets/healthcare9.png";
+import image10 from "./assets/healthcare10.png";
+import image11 from "./assets/healthcare11.png";
+import image12 from "./assets/healthcare12.png";
+import image13 from "./assets/healthcare13.png";
+import image14 from "./assets/healthcare14.png";
+import image15 from "./assets/healthcare15.png";
+import image16 from "./assets/healthcare16.png";
+import image17 from "./assets/healthcare17.png";
+import image18 from "./assets/healthcare18.png";
+import image19 from "./assets/healthcare19.png";
+import image20 from "./assets/healthcare20.png";
+import image21 from "./assets/healthcare21.png";
 
-const images = [image1, image2, image3, image4, image5, image6];
+const images = [
+  image1,
+  image2,
+  image3,
+  image4,
+  image5,
+  image6,
+  image7,
+  image8,
+  image9,
+  image10,
+  image11,
+  image12,
+  image13,
+  image14,
+  image15,
+  image16,
+  image17,
+  image18,
+  image19,
+  image20,
+  image21,
+];
 
 export default images;
