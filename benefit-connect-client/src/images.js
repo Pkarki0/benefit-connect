02f1862e0d3e1f38f7a6b5b5412a14955a1ea634
Pkarki0 <hi2carkey6@gmail.com>
2024-11-28 20:30:41 -0500

    icons added

--- benefit-connect-client/src/images.js
+++ benefit-connect-client/src/images.js
@@ -3,7 +3,8 @@ import image2 from "./assets/healthcare2.png";
 import image3 from "./assets/healthcare3.png";
 import image4 from "./assets/healthcare4.png";
 import image5 from "./assets/healthcare5.png";
+import image6 from "./assets/healthcare6.png";
 
-const images = [image1, image2, image3, image4, image5];
+const images = [image1, image2, image3, image4, image5, image6];
 
 export default images;
